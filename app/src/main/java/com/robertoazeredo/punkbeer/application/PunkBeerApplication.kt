32f3de8757e63368cd340ac8220d8304db536b39
Roberto Azeredo <robertoazdd@gmail.com>
2023-06-28 11:49:09 -0300

    Criado a classe Application e adicionado no Manifest

---
+++ app/src/main/java/com/robertoazeredo/punkbeer/application/PunkBeerApplication.kt
@@ -0,0 +1,7 @@
+package com.robertoazeredo.punkbeer.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PunkBeerApplication : Application()
