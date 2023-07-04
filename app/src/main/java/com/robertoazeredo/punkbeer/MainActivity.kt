0dd082ff2258a72149ca218fef74f4960581f3a1
Roberto Azeredo <robertoazdd@gmail.com>
2023-07-04 17:34:20 -0300

    Adicionado teste de busca da Api na Activity

--- app/src/main/java/com/robertoazeredo/punkbeer/MainActivity.kt
+++ app/src/main/java/com/robertoazeredo/punkbeer/MainActivity.kt
@@ -2,10 +2,17 @@ package com.robertoazeredo.punkbeer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.robertoazeredo.punkbeer.ui.viewmodel.BeersViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: BeersViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.getBeers()
     }
 }
