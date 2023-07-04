02e1ae026fa3a6a484f9c08f8f509f911c0609d4
Roberto Azeredo <robertoazdd@gmail.com>
2023-07-04 19:05:15 -0300

    Adicionado ajustes na MainActivity e no Manifest

--- app/src/main/java/com/robertoazeredo/punkbeer/ui/main/MainActivity.kt
+++ app/src/main/java/com/robertoazeredo/punkbeer/ui/main/MainActivity.kt
@@ -1,10 +1,13 @@
-package com.robertoazeredo.punkbeer
+package com.robertoazeredo.punkbeer.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.robertoazeredo.punkbeer.R
 import com.robertoazeredo.punkbeer.ui.viewmodel.BeersViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: BeersViewModel by viewModels()
