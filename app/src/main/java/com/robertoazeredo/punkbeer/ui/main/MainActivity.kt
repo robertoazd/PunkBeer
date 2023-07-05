e0565acb29e1b3a2631e9881dd4e282616df2176
Roberto Azeredo <robertoazdd@gmail.com>
2023-07-05 11:28:38 -0300

    Adicionado viewBinding no build e na MainActivity

--- app/src/main/java/com/robertoazeredo/punkbeer/ui/main/MainActivity.kt
+++ app/src/main/java/com/robertoazeredo/punkbeer/ui/main/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.robertoazeredo.punkbeer.ui.main
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.robertoazeredo.punkbeer.R
+import com.robertoazeredo.punkbeer.databinding.ActivityMainBinding
 import com.robertoazeredo.punkbeer.ui.viewmodel.BeersViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: BeersViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        viewModel.getBeers()
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        
     }
 }
