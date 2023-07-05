3bd5da07240160af4ea36b3b4a8975fc7691ac90
Roberto Azeredo <robertoazdd@gmail.com>
2023-07-05 11:46:16 -0300

    Criado navGraph e Fragmento de navegação

---
+++ app/src/main/java/com/robertoazeredo/punkbeer/ui/view/BeersFragment.kt
@@ -0,0 +1,19 @@
+package com.robertoazeredo.punkbeer.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.robertoazeredo.punkbeer.R
+
+class BeersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater , container: ViewGroup? ,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_beers , container , false)
+    }
+}
