be294a718aff3f19f168d7095a91913f5263fc6e
Roberto Azeredo <robertoazdd@gmail.com>
2023-07-05 11:32:43 -0300

    Adicionado navigation no build do app e do projeto

--- app/src/main/java/com/robertoazeredo/punkbeer/ui/main/MainActivity.kt
+++ app/src/main/java/com/robertoazeredo/punkbeer/ui/main/MainActivity.kt
@@ -2,11 +2,8 @@ package com.robertoazeredo.punkbeer.ui.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.robertoazeredo.punkbeer.R
 import com.robertoazeredo.punkbeer.databinding.ActivityMainBinding
-import com.robertoazeredo.punkbeer.ui.viewmodel.BeersViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-        
+
     }
 }
