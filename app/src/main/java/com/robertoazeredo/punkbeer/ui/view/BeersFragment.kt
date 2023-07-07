45f3bbf0a047cc7024157c903554d313de2d0d7c
Roberto Azeredo <robertoazdd@gmail.com>
2023-07-07 12:11:45 -0300

    Adicionado viewModel no BeersFragment

--- app/src/main/java/com/robertoazeredo/punkbeer/ui/view/BeersFragment.kt
+++ app/src/main/java/com/robertoazeredo/punkbeer/ui/view/BeersFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.robertoazeredo.punkbeer.R
 import com.robertoazeredo.punkbeer.databinding.FragmentBeersBinding
+import com.robertoazeredo.punkbeer.ui.viewmodel.BeersViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class BeersFragment : Fragment() {
 
     private lateinit var binding: FragmentBeersBinding
+    private val viewModel: BeersViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
@@ -21,5 +24,12 @@ class BeersFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentBeersBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
+        super.onViewCreated(view , savedInstanceState)
+        if (viewModel.beers.value == null) {
+            viewModel.getBeers()
+        }
     }
 }
