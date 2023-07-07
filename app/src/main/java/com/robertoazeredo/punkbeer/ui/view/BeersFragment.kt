0b9b64362214eaa9fd8ecf888a0af1d265e026fa
Roberto Azeredo <robertoazdd@gmail.com>
2023-07-07 12:09:37 -0300

    Adicionado binding no BeersFragment

--- app/src/main/java/com/robertoazeredo/punkbeer/ui/view/BeersFragment.kt
+++ app/src/main/java/com/robertoazeredo/punkbeer/ui/view/BeersFragment.kt
@@ -6,14 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.robertoazeredo.punkbeer.R
+import com.robertoazeredo.punkbeer.databinding.FragmentBeersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class BeersFragment : Fragment() {
+
+    private lateinit var binding: FragmentBeersBinding
 
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beers , container , false)
+        binding = FragmentBeersBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
