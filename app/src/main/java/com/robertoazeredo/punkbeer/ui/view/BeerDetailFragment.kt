fe0583e4796342cfb7fbcaa19c264cd1e4cbf064
Roberto Azeredo <robertoazdd@gmail.com>
2023-07-10 11:08:19 -0300

    Adicionado args no BeerDetailFragment

--- app/src/main/java/com/robertoazeredo/punkbeer/ui/view/BeerDetailFragment.kt
+++ app/src/main/java/com/robertoazeredo/punkbeer/ui/view/BeerDetailFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.robertoazeredo.punkbeer.R
 
 class BeerDetailFragment : Fragment() {
+
+    private val args: BeerDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
@@ -15,5 +18,10 @@ class BeerDetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_beer_detail , container , false)
+    }
+
+    override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
+        super.onViewCreated(view , savedInstanceState)
+        println(args.beer)
     }
 }
