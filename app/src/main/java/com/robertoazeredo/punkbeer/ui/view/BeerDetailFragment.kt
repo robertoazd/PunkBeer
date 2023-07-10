1a3cb16d95099bd12f8ce91dcced3a4f16f27bb0
Roberto Azeredo <robertoazdd@gmail.com>
2023-07-10 12:51:36 -0300

    Adicionado função setupLayout no BeerDetailFragment e ajuste no Layout

--- app/src/main/java/com/robertoazeredo/punkbeer/ui/view/BeerDetailFragment.kt
+++ app/src/main/java/com/robertoazeredo/punkbeer/ui/view/BeerDetailFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.robertoazeredo.punkbeer.R
 import com.robertoazeredo.punkbeer.databinding.FragmentBeerDetailBinding
 
 class BeerDetailFragment : Fragment() {
@@ -24,6 +26,31 @@ class BeerDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
         super.onViewCreated(view , savedInstanceState)
-        println(args.beer)
+
+        setupLayout()
+    }
+
+    private fun setupLayout() {
+
+        val beer = args.beer
+        Glide.with(binding.root.context).load(beer.imageUrl).into(binding.ivBeer)
+
+        binding.tvName.text = getString(R.string.name, beer.name)
+
+        if (!beer.tagline.isNullOrEmpty()) {
+            binding.tvTagline.text = getString(R.string.tagline, beer.tagline)
+        } else {
+            binding.tvTagline.visibility = View.GONE
+        }
+
+        if (!beer.description.isNullOrEmpty()) {
+            binding.tvDescription.text = getString(R.string.description , beer.description)
+        } else {
+            binding.tvDescription.visibility = View.GONE
+        }
+
+        binding.tvFirstBrewed.text = getString(
+            R.string.first_brewed_format, beer.firstBrewed)
+
     }
 }
