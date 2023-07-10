c5f2fd6c0841f01bd72f303c959e0e32b4000649
Roberto Azeredo <robertoazdd@gmail.com>
2023-07-10 10:10:08 -0300

    Ajuste no nav_graph e adicionado navegação do BeersFragment para Detalhes da Beer

--- app/src/main/java/com/robertoazeredo/punkbeer/ui/view/BeersFragment.kt
+++ app/src/main/java/com/robertoazeredo/punkbeer/ui/view/BeersFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.SnapHelper
 import com.robertoazeredo.punkbeer.databinding.FragmentBeersBinding
@@ -53,7 +54,9 @@ class BeersFragment : Fragment() {
 
     private fun setupListeners() {
         adapter.itemClick = { beer ->
-            println("Criar fragment de detalhes da beer")
+            findNavController().navigate(
+                BeersFragmentDirections.actionBeersFragmentToBeerDetailFragment()
+            )
         }
     }
 
