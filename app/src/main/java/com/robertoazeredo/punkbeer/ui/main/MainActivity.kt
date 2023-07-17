1d8c0042cf459049e379a002a1556e5ca93d7cc0
Roberto Azeredo <robertoazdd@gmail.com>
2023-07-17 17:07:57 -0300

    Adicionado installSplashScreen no onCreate

--- app/src/main/java/com/robertoazeredo/punkbeer/ui/main/MainActivity.kt
+++ app/src/main/java/com/robertoazeredo/punkbeer/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.robertoazeredo.punkbeer.ui.main
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.robertoazeredo.punkbeer.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,9 +11,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-
     }
 }
